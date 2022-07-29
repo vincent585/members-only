6dbaa8c6196f33521f4835b68cb596afc77784a8
Vincent Klimjack <vklimjack@gmail.com>
2022-07-29 16:59:19 +0100

    add name and username to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_154318) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_155740) do
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
@@ -26,8 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_154318) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
