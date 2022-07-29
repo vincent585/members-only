61358f5906553064719600e2511047b2bdc1a08b
Vincent Klimjack <vklimjack@gmail.com>
2022-07-29 16:51:57 +0100

    basic setup of models and devise plus migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'posts#index'
+
+  resources :posts, only: %i[index new create]
 end
