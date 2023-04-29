528a9b0a116761c056b7f5a85c56866be50fc909
Vincent Klimjack <vklimjack@gmail.com>
2023-04-29 10:40:28 +0100

    add username to signup form, add devise views, add New post form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,13 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only [:new, :create]
+  before_action :authenticate_user!, only: %i[new create]
+
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
 end
